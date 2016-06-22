834c1d309af0d6d8af1ac31784a6ae5168cd8e34
diochagas23 <diogeneschagas20@gmail.com>
2016-06-22 15:07:28 -0300

    New_updates

--- app/src/main/java/com/modacenter/modacenterbeta/ContatoActivity.java
+++ app/src/main/java/com/modacenter/modacenterbeta/ContatoActivity.java
@@ -1,12 +1,10 @@
 package com.modacenter.modacenterbeta;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
