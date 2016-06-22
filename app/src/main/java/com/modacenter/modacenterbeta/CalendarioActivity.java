f1cee15156a370a93390b70384691edf0f49e0ac
diochagas23 <diogeneschagas20@gmail.com>
2016-06-22 16:50:25 -0300

    CalendarioUpdate

--- app/src/main/java/com/modacenter/modacenterbeta/CalendarioActivity.java
+++ app/src/main/java/com/modacenter/modacenterbeta/CalendarioActivity.java
@@ -2,12 +2,32 @@ package com.modacenter.modacenterbeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CalendarioActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
+        TextView textView = (TextView) findViewById(R.id.Tvcliqueaqui);
+        ImageView imageView = (ImageView) findViewById(R.id.calendar);
+
+
+
+        Toolbar calendario = (Toolbar) findViewById(R.id.tbcalendario);
+        setSupportActionBar(calendario);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
+
+
+
+
 }
